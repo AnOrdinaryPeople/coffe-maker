52b2f1be801b0463303ccd451a44b64a7d874421
Muhammad Dafitra Ar-Rasyid <m.dafitra@smt-id.com>
2021-08-14 20:36:31 +0700

    ⚡️ Simplify some code

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from data import MENU, resources
-from operator import itemgetter
 from lang.id import lang
 import re
 
@@ -13,18 +12,22 @@ menu_coin = [
 ]
 
 
+def throw(message):
+    raise Exception(message)
+
+
 def choose_coffee():
     try:
         user_input = input(lang['choose_coffee'](menu_keys)).lower()
 
         if user_input.isnumeric():
-            raise Exception(lang['errors']['must_char'])
+            throw(lang['errors']['must_char'])
         elif user_input == '':
-            raise Exception(lang['errors']['empty_input'])
+            throw(lang['errors']['empty_input'])
         elif re.search('[@_!#$%^&*()<>?/\|}{~:]', user_input):
-            raise Exception(lang['errors']['symbols'])
+            throw(lang['errors']['symbols'])
         elif not (user_input in menu_keys):
-            raise Exception(lang['errors']['not_found'](user_input))
+            throw(lang['errors']['not_found'](user_input))
     except Exception as error:
         raise error
 
@@ -40,7 +43,7 @@ def input_money():
         try:
             money.append(int(input(lang['input_coin_list'](m[0]))) * m[1])
         except:
-            raise Exception(lang['errors']['must_numeric'])
+            throw(lang['errors']['must_numeric'])
 
     return round(sum(money), 2)
 
@@ -52,7 +55,7 @@ def validate_money(total_money):
         total_money -= selected['cost']
         print(lang['return'], total_money)
     else:
-        raise Exception(lang['errors']['not_enough'])
+        throw(lang['errors']['not_enough'])
 
 
 def coffee_maker(inputuser, object):
@@ -80,9 +83,8 @@ while boolean:
     try:
         user_input = choose_coffee()
         selected = MENU[user_input]
-        total_money = input_money()
 
-        validate_money(total_money)
+        validate_money(input_money())
 
         print(lang['result'], coffee_maker(user_input, selected))
 
